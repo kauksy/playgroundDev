81cc21c2b191000c13219bbe9455ce5b59a6c357
kauksy <dev.mindfulness02@gmail.com>
2023-03-27 23:33:20 +0900

    [bookstore2] 도서정보 삽입 페이지 추가

--- bookstore2/src/main/java/com/play/jcslib/bookstore2/controller/BookController.java
+++ bookstore2/src/main/java/com/play/jcslib/bookstore2/controller/BookController.java
@@ -17,15 +17,29 @@ public class BookController {
     @Autowired
     private BookService bookService;
      
+    /**
+     * /index url로 접속하면
+     * temlplates > book 하위에 있는 index.html의 화면이 수행 됨
+     * book/index
+     **/
     @RequestMapping(value= "/index")
     public String index(Model model){
         List<BookVo> bookList = bookService.getAllBooks();
         model.addAttribute("bookList", bookList);
         return "book/index";
     }
- 
-    @RequestMapping(value= "/index1")
-    public String index1(Model model){
-        return "testindex";
+
+    /**
+     * /makebookform url로 접속하면 (book/index의 버튼을 클릭하면 /makebookform 으러 넘어감)
+     * temlplates > book 하위에 있는 makebookform.html의 화면이 수행 됨
+     * book/makebookform
+     **/
+    @RequestMapping(value= "/makebookform")
+    public String makebookform(Model model){
+
+        return "book/makebookform";
     }
+
+
+
 }
