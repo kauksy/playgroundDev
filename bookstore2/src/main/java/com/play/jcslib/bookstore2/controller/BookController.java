bb956cfdf45700fde3cd17e0aebf67e63aaac73e
kauksy <dev.syeon@gmail.com>
2023-04-04 02:15:19 +0900

    [bookstore2] 불필요한 import 제거

--- bookstore2/src/main/java/com/play/jcslib/bookstore2/controller/BookController.java
+++ bookstore2/src/main/java/com/play/jcslib/bookstore2/controller/BookController.java
@@ -1,17 +1,13 @@
 package com.play.jcslib.bookstore2.controller;
 
-import java.net.http.HttpClient.Redirect;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.play.jcslib.bookstore2.service.BookService;
 import com.play.jcslib.bookstore2.vo.BookVo;
